9c9598c7a940ce2d23118c0b603ed681bc6e96cf
rishatl <68281667+rishatl@users.noreply.github.com>
2024-03-03 14:01:43 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftHTMLtoMarkdown",
     platforms: [
-        .iOS(.v16),
+        .iOS(.v15),
         .macOS(.v13)
     ],
     products: [
