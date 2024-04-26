a3db8679150a78343c7db61b32962597f430240e
Ryan Booker <7423+ryanbooker@users.noreply.github.com>
2024-04-26 21:09:58 +1000

    Bump support iOS version to fix compile errors on iOS

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftHTMLtoMarkdown",
     platforms: [
-        .iOS(.v15),
+        .iOS(.v16),
         .macOS(.v13)
     ],
     products: [
